d2224dc9be37e105834504b4ac813c3238561d51
Joffrey Trn <101974770+MentalDeFer972@users.noreply.github.com>
2022-07-09 19:35:08 +0200

    commitForLinkAlamofireToAPI

---
+++ TheRightBaluchon/Pods/Target Support Files/Pods-TheRightBaluchon/Pods-TheRightBaluchon-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TheRightBaluchon : NSObject
+@end
+@implementation PodsDummy_Pods_TheRightBaluchon
+@end
--- TheRightBaluchon/TheRightBaluchon/ViewController.swift
+++ TheRightBaluchon/TheRightBaluchon/ViewController.swift
@@ -6,14 +6,80 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
 
+class WeatherController: UIViewController {
+    
+    @IBOutlet weak var city: UILabel!
+    
+    @IBOutlet weak var imageCondition: UIImageView!
+    
+    @IBOutlet weak var temperature: UILabel!
+    
+    @IBOutlet weak var changeCityConfig: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        AF.request("https://api.openweathermap.org/data/2.5/weather?q=\(String(describing: city.text))&appid=5fb97e1a17e403be063cc0b941ea6e08")
+            .responseDecodable(of: [Weather].self){
+                (reponse) in
+                        
+            }
     }
+}
+
+struct Weather : Decodable{
+    
+    var id : String?
+    var main : String?
+    var city : String?
+    var condition : String?
+    var temperature : String?
+    
+    init(json: [String: Any]){
+        id = json["id"] as? String
+        main = json["main"] as? String
+        city = json["city"] as? String
+        condition = json["condition"] as? String
+        temperature = json["temperature"] as? String
+    }
+    
+}
 
 
+class TraduceController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+}
+
+struct Traduce : Decodable{
+    
+}
+
+
+
+class MoneyController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+}
+
+struct Money : Decodable{
+    
 }
 
